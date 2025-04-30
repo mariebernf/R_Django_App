2877c690e9d5267b69951535f47a8975fe102a4a
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-04-30 23:54:06 +0200

    Add dynamic recipe data and display it on the home page

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, HttpResponse
 
+recipes = [
+    {
+        'author': 'Dom',
+        'title': 'meatball sub',
+        'directions': 'combine all ingredients',
+        'date_posted': 'May 19, 2022',
+    },
+    {
+        'author': 'Dom',
+        'title': 'turkey sub',
+        'directions': 'combine all ingredients',
+        'date_posted': 'May 10, 2022',
+    },
+    {
+        'author': 'Dom',
+        'title': 'ham & cheese sub',
+        'directions': 'combine all ingredients',
+        'date_posted': 'May 07, 2022',
+    },
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to my Recipes App!</h1>")
+    context = {
+        'recipes': recipes
+    }
+    return render(request, "recipes/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>This is a recipes app to keep track of your recipes</h1>")
+    return render(request, "recipes/about.html")
